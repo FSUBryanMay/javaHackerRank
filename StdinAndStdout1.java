b85161c02c87f7b68ebba551ba7c8eac0236c2cc
FSUBryanMay <bmmay0@frostburg.edu>
2018-03-22 19:35:30 -0400

    Provide solution to Java Stdin And Stdout 1

---
+++ StdinAndStdout1.java
@@ -0,0 +1,10 @@
+public class StdinAndStdout1{
+    public static void main(String[] args){
+        Scanner console = new Scanner(System.in);
+
+        for(int i = 0; i < 3; i++){
+            int integer = scan.nextInt();
+            System.out.println(integer);
+        }
+    }
+}
