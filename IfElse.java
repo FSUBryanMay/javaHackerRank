91674ca2c82de3cb9c7af05588558ed6d74f58b9
FSUBryanMay <bmmay0@frostburg.edu>
2018-03-22 19:45:12 -0400

    Provide solution to Java If-Else

---
+++ IfElse.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class IfElse{
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int n = console.nextInt();
+
+        if((n % 2 == 1) || ((n % 2 == 0) && ((n >= 6) && (n <= 20)))){
+            System.out.println("Weird");
+        }
+        else{
+            System.out.println("Not Weird");
+        }
+    }
+}
